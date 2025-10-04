858ea432a222c116a2f897f2467d662890b7d611
Black Cat <luan99tth@gmail.com>
2025-10-04 15:46:11 +0700

    - remove progressbar

--- videoDownloader.py
+++ videoDownloader.py
@@ -3,9 +3,11 @@ import subprocess
 import threading
 from tkinter import ttk
 
-STATUS_ROW = 20
+LOG_ROW = 20
+STATUS_ROW = 21
 
 window = tk.Tk()
+window.iconbitmap("icon.ico")
 window.title("Video Downloader")
 window.geometry("400x400")
 
@@ -29,26 +31,26 @@ urlString = tk.StringVar()
 urlLabel = tk.Label(window, text="URL: ").grid(row=10, sticky=tk.W, padx=5)
 urlEntry = tk.Entry(window, textvariable=urlString, width=50).grid(row=11, sticky=tk.W, padx=5)
 
-def on_download_done(progressbar:ttk.Progressbar):
+def on_download_done():
     reset_state()
-    progressbar.stop()
-    progressbar.destroy()
-
     print("Finished!" )
     tk.Label(window, text="FINISHED: Downloaded Video", fg="green").grid(row=STATUS_ROW, sticky=tk.W)
 
-def on_download_err(progressbar:ttk.Progressbar, err:str):
+def on_download_err(err:str):
     reset_state()
-    progressbar.stop()
-    progressbar.destroy()
-
     tk.Label(window, text=err, fg="red").grid(row=STATUS_ROW, sticky=tk.W)
 
 def reset_state():
     isDownloading = False
     downloadButton.config(state=tk.NORMAL)
 
-def download_video(command:str, progressbar:ttk.Progressbar, window):
+def clean_status():
+    tk.Label(window, text=" ", width=100).grid(row=STATUS_ROW, sticky=tk.W, ipadx=10, ipady=10)
+
+def clean_log():
+    tk.Label(window, text=" ", width=100).grid(row=LOG_ROW, sticky=tk.W, ipadx=10, ipady=10)
+
+def download_video(command:str, window):
     hasException = False
     
     try:
@@ -59,27 +61,29 @@ def download_video(command:str, progressbar:ttk.Progressbar, window):
     if (process.stdout is not None):
         for line in process.stdout:
             print(line, end="")
-            tk.Label(window, text=line, fg="black").grid(row=STATUS_ROW, sticky=tk.W)
+            clean_log()
+            tk.Label(window, text=line, fg="black").grid(row=LOG_ROW, sticky=tk.W)
 
     if (process.stderr is not None):
         for line in process.stderr:
             print(line, end="")
-            tk.Label(window, text=line, fg="red").grid(row=STATUS_ROW, sticky=tk.W)
+            clean_log()
+            tk.Label(window, text=line, fg="red").grid(row=LOG_ROW, sticky=tk.W)
         hasException = True
 
     process.wait()
 
     if process.returncode != 0 and hasException:
-        window.after(0, lambda: on_download_err(progressbar, "Error Occured!"))
+        window.after(0, lambda: on_download_err("Error Occured!"))
     else:
-        window.after(0, lambda: on_download_done(progressbar))
+        window.after(0, lambda: on_download_done())
 
 
 def download_video_runner():
     isDownloading = True
     downloadButton.config(state=tk.DISABLED)
     print("Repairing to download...")
-    tk.Label(window, text="", width=100).grid(row=STATUS_ROW, sticky=tk.W)
+    clean_status()
     command = 'yt-dlp'
 
     if usingVrVideoClient.get():
@@ -111,14 +115,11 @@ def download_video_runner():
     print("downloading video, url: " + command)
 
     try:
-        progressbar = ttk.Progressbar(mode="indeterminate", orient=tk.HORIZONTAL, length=200)
-        progressbar.grid(row=19, sticky=tk.W)
-        progressbar.start(10)
-
-        threading.Thread(target=download_video, args=(command, progressbar, window), daemon=True).start()
+        threading.Thread(target=download_video, args=(command, window), daemon=True).start()
 
     except:
         print("[EXCEPTION]" )
+        clean_status()
         tk.Label(window, text="Exception when downloading video", fg="red").grid(row=STATUS_ROW, sticky=tk.W)
         reset_state()
         return
